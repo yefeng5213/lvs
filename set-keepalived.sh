49efed1c962b6f0c01ebc48e8a24b971844e5175
yefeng5213 <541812516@qq.com>
2018-11-27 01:24:46 +0800

    version 1.0.4

--- set-keepalived.sh
+++ set-keepalived.sh
@@ -3,13 +3,17 @@
 master () {
 rpm -q keepalived &> /dev/null
 if [ $? -ne 0 ];then 
-	yum -y install keepalived && \cp ./keepalived.conf ./notify.sh /etc/keepalived/ && systemctl start keepalived.service
+	echo "keepalived installing ..."
+	yum -y install keepalived &> /dev/null && \cp ./keepalived.conf ./notify.sh /etc/keepalived/ && systemctl start keepalived.service &> /dev/null 
+	echo "keepalived installd OK"
 	else
-	\cp ./keepalived.conf ./notify.sh /etc/keepalived/ && systemctl start keepalived.service
+	echo "keepalived is existed"
+	\cp ./keepalived.conf ./notify.sh /etc/keepalived/ && systemctl start keepalived.service &> /dev/null
 fi
+yum -y install ipvsadm &> /dev/null
 }
 backup () {
-master 
+master
 sed -i "s@router_id node1@router_id node2@g" /etc/keepalived/keepalived.conf &> /dev/null
 sed -i "s@state MASTER@state BACKUP@g" /etc/keepalived/keepalived.conf &> /dev/null
 sed -i "s@priority 100@priority 96@g" /etc/keepalived/keepalived.conf &> /dev/null
@@ -17,9 +21,11 @@ systemctl start keepalived.service
 }
 case $1 in 
 MASTER)
-	master;;
+	master MASTER
+	;;
 BACKUP)
-	backup;;
+	backup BACKUP
+	;;
 *)
 	echo "you must write {MASTER|BACKUP}"
 	;;
