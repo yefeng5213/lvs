82625ab99fc5d65bc35da0e5338d30ecf642ed61
yefeng5213 <541812516@qq.com>
2018-11-26 23:32:59 +0800

    version 1.0.1

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# lvs
+# lvs-dr
 lvs-dr模式中通过脚本修改内核参数以现在RS的ARP通告和应答级别
+lvs-director.sh 设置director的vip地址
+set-dr-arp.sh 设置rs的rip地址和内核参数
--- lvs-director.sh
+++ lvs-director.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 #设置lvs-director的ip地址和内核参数
-ifconfig ens33:0 192.168.43.99 netmask 255.255.255.255 broadcast 192.168.43.99 && sysctl -t net.ipv4.ip_forward=1
+vip=192.168.43.99
+nk=255.255.255.255
+ifconfig | grep "ens33:0" &> /dev/null
+if [ $? -ne 0 ];then 
+	ifconfig ens33:0 192.168.43.99 netmask 255.255.255.255 broadcast 192.168.43.99 && sysctl -w net.ipv4.ip_forward=1
+	else 
+	exit 1
+	echo "ens33:0 already exist"
+fi
