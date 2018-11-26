e66d833fb323f69cfe7de15fc456cd3d35db4522
yefeng5213 <541812516@qq.com>
2018-11-27 00:49:05 +0800

    version 1.0.3

--- set-keepalived.sh
+++ set-keepalived.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
 #
-yum -y install keepalived && cp ./keepalived.conf /etc/keepalived/keepalived.conf
+master () {
+rpm -q keepalived &> /dev/null
+if [ $? -ne 0 ];then 
+	yum -y install keepalived && \cp ./keepalived.conf ./notify.sh /etc/keepalived/ && systemctl start keepalived.service
+	else
+	\cp ./keepalived.conf ./notify.sh /etc/keepalived/ && systemctl start keepalived.service
+fi
+}
+backup () {
+master 
 sed -i "s@router_id node1@router_id node2@g" /etc/keepalived/keepalived.conf &> /dev/null
 sed -i "s@state MASTER@state BACKUP@g" /etc/keepalived/keepalived.conf &> /dev/null
 sed -i "s@priority 100@priority 96@g" /etc/keepalived/keepalived.conf &> /dev/null
 systemctl start keepalived.service
+}
+case $1 in 
+MASTER)
+	master;;
+BACKUP)
+	backup;;
+*)
+	echo "you must write {MASTER|BACKUP}"
+	;;
+esac
